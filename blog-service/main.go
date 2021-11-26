00a65bab95bda767d1963eb4ccdf56736d761a80
Rancho <rancho.z@binance.com>
2021-11-26 14:46:19 +0800

    clear blog-service

--- blog-service/main.go
+++ blog-service/main.go
@@ -1,101 +1,10 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"log"
-	"net/http"
-	"time"
+/**
+ * @author Rancho
+ * @date 2021/11/26
+ */
 
-	"github.com/gin-gonic/gin"
-	"gopkg.in/natefinch/lumberjack.v2"
-
-	"github.com/RanchoCooper/go-programming-tour-book/blog-service/configs"
-	_ "github.com/RanchoCooper/go-programming-tour-book/blog-service/docs"
-	"github.com/RanchoCooper/go-programming-tour-book/blog-service/global"
-	"github.com/RanchoCooper/go-programming-tour-book/blog-service/internal/model"
-	"github.com/RanchoCooper/go-programming-tour-book/blog-service/internal/routers"
-	"github.com/RanchoCooper/go-programming-tour-book/blog-service/pkg/logger"
-)
-
-func init() {
-	// init setting
-	err := setupSetting()
-	if err != nil {
-		log.Fatalf("init.setupSetting err: %v", err)
-	}
-
-	// init database
-	err = setupDBEngine()
-	if err != nil {
-		log.Fatalf("init.setupDBEngine err: %v", err)
-	}
-
-	// init logger
-	err = setupLogger()
-	if err != nil {
-		log.Fatalf("init.setupLogger err: %v", err)
-	}
-}
-
-// @title 博客系统
-// @version 1.0
-// @description Go 语言编程之旅：一起用 Go 做项目
 func main() {
-	gin.SetMode(global.ServerSetting.RunMode)
-	router := routers.NewRouter()
-	s := &http.Server{
-		Addr:           ":" + global.ServerSetting.HttpPort,
-		Handler:        router,
-		ReadTimeout:    global.ServerSetting.ReadTimeout * time.Second,
-		WriteTimeout:   global.ServerSetting.WriteTimeout * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-	_ = s.ListenAndServe()
-}
-
-func setupSetting() error {
-	setting, err := configs.NewSetting()
-	if err != nil {
-		return nil
-	}
-	err = setting.ReadSection("Server", &global.ServerSetting)
-	if err != nil {
-		return nil
-	}
-	err = setting.ReadSection("App", &global.AppSetting)
-	if err != nil {
-		return nil
-	}
-	err = setting.ReadSection("Database", &global.DatabaseSetting)
-	if err != nil {
-		return nil
-	}
-	fmt.Printf("ServerSetting: %v\nAppSetting: %v\nDatabaseSetting: %v\n", global.ServerSetting, global.AppSetting, global.DatabaseSetting)
-	err =setting.ReadSection("JWT", &global.JWTSetting)
-	if err != nil {
-		return err
-	}
-	global.JWTSetting.Expire *= time.Second
-	return nil
-}
-
-func setupDBEngine() error {
-	var err error
-	global.DBEngine, err = model.NewDBEngine(*global.DatabaseSetting)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func setupLogger() error {
-	global.Logger = logger.NewLogger(&lumberjack.Logger{
-		Filename:  global.AppSetting.LogSavePath + "/" + global.AppSetting.LogFileName + global.AppSetting.LogFileExt,
-		MaxSize:   600,
-		MaxAge:    10,
-		LocalTime: true,
-	}, "", log.LstdFlags).WithCaller(2)
-	global.Logger.Infof(context.Background(), "%s: go-programming-tour-book/%s", "rancho", "blog-service")
-	return nil
+    
 }
--- go.mod
+++ go.mod
@@ -10,19 +10,21 @@ require (
 	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/gin-gonic/gin v1.6.3
+	github.com/gin-gonic/gin v1.7.7
 	github.com/go-openapi/spec v0.20.3 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
-	github.com/go-playground/locales v0.13.0
-	github.com/go-playground/universal-translator v0.17.0
+	github.com/go-playground/locales v0.14.0
+	github.com/go-playground/universal-translator v0.18.0
 	github.com/go-playground/validator v9.31.0+incompatible
-	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
 	github.com/jonboulle/clockwork v0.1.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/prometheus/client_golang v0.9.3 // indirect
 	github.com/soheilhy/cmux v0.1.4 // indirect
@@ -35,14 +37,14 @@ require (
 	github.com/swaggo/gin-swagger v1.3.0
 	github.com/swaggo/swag v1.6.7
 	github.com/tmc/grpc-websocket-proxy v0.0.0-20190109142713-0ad062ec5ee5 // indirect
-	github.com/ugorji/go v1.2.4 // indirect
+	github.com/ugorji/go v1.2.6 // indirect
 	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
 	go.etcd.io/bbolt v1.3.2 // indirect
-	golang.org/x/crypto v0.0.0-20210314154223-e6e6c4f2bb5b // indirect
-	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
-	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/grpc v1.38.0
-	google.golang.org/protobuf v1.26.0
+	google.golang.org/protobuf v1.27.1
 	gopkg.in/go-playground/validator.v9 v9.31.0
 	gopkg.in/ini.v1 v1.62.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
