02fe5e63b9ecd20a66066120c7899330b4b10e46
Rancho <rancho.z@binance.com>
2021-11-26 14:59:54 +0800

    2.1 basic tour

--- blog-service/main.go
+++ blog-service/main.go
@@ -1,10 +1,20 @@
 package main
 
+import (
+    "github.com/gin-gonic/gin"
+)
+
 /**
  * @author Rancho
  * @date 2021/11/26
  */
 
 func main() {
-    
+   r := gin.Default()
+
+   r.GET("/ping", func(context *gin.Context) {
+       context.JSON(200, gin.H{"message": "pong"})
+   })
+
+   r.Run(":8888")
 }
