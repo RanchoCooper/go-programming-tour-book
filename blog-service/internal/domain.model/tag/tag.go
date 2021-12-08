646c68b82f9b005c44a434ded4959f5e5603ee8c
Rancho <rancho.z@binance.com>
2021-12-08 18:28:25 +0800

    up

--- blog-service/internal/domain.model/tag/tag.go
+++ blog-service/internal/domain.model/tag/tag.go
@@ -1,8 +1,6 @@
 package tag
 
 import (
-    "errors"
-
     "gorm.io/gorm"
 )
 
@@ -19,65 +17,4 @@ type Tag struct {
 
 func (t Tag) TableName() string {
     return "blog_tag"
-}
-
-func (t Tag) Count(db *gorm.DB) (int64, error) {
-    var count int64
-    if t.Name != "" {
-        db = db.Where("name = ?", t.Name)
-    }
-    db = db.Where("state = ?", t.State)
-    err := db.Model(&t).Where("is_del = ?", 0).Count(&count).Error
-    if err != nil {
-        return 0, err
-    }
-
-    return count, nil
-}
-
-func (t Tag) Get(db *gorm.DB) (*Tag, error) {
-    var tag *Tag
-    var err error
-    if t.ID == 0 {
-        return nil, errors.New("ID is empty")
-    }
-    err = db.Table(t.TableName()).Take(tag).Error
-    if errors.Is(err, gorm.ErrRecordNotFound) {
-        return nil, nil
-    }
-    if err != nil {
-        return nil, err
-    }
-
-    return tag, nil
-}
-
-func (t Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
-    var tags []*Tag
-    var err error
-
-    if pageOffset >= 0 && pageSize > 0 {
-        db = db.Offset(pageOffset).Limit(pageSize)
-    }
-    if t.Name != "" {
-        db = db.Where("name = ?", t.Name)
-    }
-    db = db.Where("state = ?", t.State)
-    if err = db.Where("is_del = ?", 0).Find(&tags).Error; err != nil {
-        return nil, err
-    }
-
-    return tags, nil
-}
-
-func (t Tag) Create(db *gorm.DB) error {
-    return db.Create(&t).Error
-}
-
-func (t Tag) Update(db *gorm.DB, values interface{}) error {
-    return db.Model(&t).Where("id = ? AND is_del = ?", t.ID, 0).Updates(values).Error
-}
-
-func (t Tag) Delete(db *gorm.DB) error {
-    return db.Where("id = ? AND is_del = ?", t.Model.ID, 0).Delete(&t).Error
 }
--- blog-service/internal/port.adapter/repository/mysql/tag.go
+++ blog-service/internal/port.adapter/repository/mysql/tag.go
@@ -12,12 +12,31 @@ import (
  */
 
 type ITag interface {
+    CreateTag(*tag.Tag) (*tag.Tag, error)
+    DeleteTag(int64) (int64, error)
+    UpdateTag(where map[string]interface{}, update map[string]interface{}) (int64, error)
     GetTag(uint32, uint8) (*tag.Tag, error)
     GetTagList(string, uint32, int) ([]*tag.Tag, error)
+    CountTag() (int64, error)
 }
 
 type TagRepo struct {
     db *gorm.DB
+}
+
+func (t TagRepo) CreateTag(t2 *tag.Tag) (*tag.Tag, error) {
+    err := t.db.Create(t2).Error
+    return t2, err
+}
+
+func (t TagRepo) DeleteTag(i int64) (int64, error) {
+    // TODO implement me
+    panic("implement me")
+}
+
+func (t TagRepo) UpdateTag(where map[string]interface{}, update map[string]interface{}) (int64, error) {
+    // TODO implement me
+    panic("implement me")
 }
 
 func (t TagRepo) GetTag(u uint32, u2 uint8) (*tag.Tag, error) {
@@ -26,6 +45,11 @@ func (t TagRepo) GetTag(u uint32, u2 uint8) (*tag.Tag, error) {
 }
 
 func (t TagRepo) GetTagList(s string, u uint32, i int) ([]*tag.Tag, error) {
+    // TODO implement me
+    panic("implement me")
+}
+
+func (t TagRepo) CountTag() (int64, error) {
     // TODO implement me
     panic("implement me")
 }
