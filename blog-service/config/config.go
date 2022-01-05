218b2bbd39507e9193640548e23f2976046a7e9a
Rancho <rancho.z@binance.com>
2022-01-06 03:01:56 +0800

    paginate and patch config

--- blog-service/config/config.go
+++ blog-service/config/config.go
@@ -26,10 +26,12 @@ type appConfig struct {
 }
 
 type httpServerConfig struct {
-    Addr         string `yaml:"addr"`
-    Pprof        bool   `yaml:"pprof"`
-    ReadTimeout  string `yaml:"read_timeout"`
-    WriteTimeout string `yaml:"write_timeout"`
+    Addr            string `yaml:"addr"`
+    Pprof           bool   `yaml:"pprof"`
+    DefaultPageSize int    `yaml:"default_page_size"`
+    MaxPageSize     int    `yaml:"max_page_size"`
+    ReadTimeout     string `yaml:"read_timeout"`
+    WriteTimeout    string `yaml:"write_timeout"`
 }
 
 type logConfig struct {
